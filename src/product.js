bee0d8cc1ca54f62c05ee58b5a50f1a8a7c9aca7
Temiloluwa Gboyega <temiloluwagboyega@gmail.com>
2024-07-09 21:04:13 +0100

    changes

--- src/product.js
+++ src/product.js
@@ -8,28 +8,28 @@ import prod4 from './Components/assets/headset4.png'
 export const PRODUCTS = [
     {
         id: 1,
-        productName: 'Industrual Bed',
+        productName: 'Industrial Bed',
         price: 300.0,
         productImage: prod1,
         description: 'Organic Cotton, fairtrades certified'
     },
     {
         id : 2,
-        productName: 'Industrual Bed',
+        productName: 'Industrial Bed',
         price: 300.0,
         productImage: prod2,
         description: 'Organic Cotton, fairtrades certified'
     },
     {
         id : 3,
-        productName: 'Industrual Bed',
+        productName: 'Industrial Bed',
         price: 300.0,
         productImage: prod3,
         description: 'Organic Cotton, fairtrades certified'
     },
     {
         id : 4,
-        productName: 'Industrual Bed',
+        productName: 'Industrial Bed',
         price: 300.0,
         productImage: prod4,
         description: 'Organic Cotton, fairtrades certified'
